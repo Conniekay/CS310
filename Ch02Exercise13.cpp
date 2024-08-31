da2df404b8d21820499304cef52656692555ffc0
Connie Smith <connie.kay.smith@gmail.com>
2024-08-31 16:36:17 -0500

    Update Ch02Exercise13.cpp
    Wrote program that reads the original price, the mark up percentage and the sales tax rate of an item
    then outputs the original price, the percentage of mark up, the selling price, the sales tax rate,
    and the final price of the item.

--- Ch02Exercise13.cpp
+++ Ch02Exercise13.cpp
@@ -1,1 +1,45 @@
+/*
+This program reads the original price, the mark up percentage, and the sales tax rate of an item
+then outputs the original price, the percentage of mark up, the selling price, the sales tax rate,
+and the final price of the item.
+/*
 
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	double originalPrice;
+	double markupPercentage;
+	double salesTaxRate;
+
+	cout << "Enter the original price of the item: ";
+	cin >> originalPrice;
+
+   	cout << "Enter the percentage markup: ";
+    	cin >> markupPercentage;
+
+    	cout << "Enter the sales tax rate: ";
+  	cin >> salesTaxRate;
+
+    	// Calculate the selling price after markup
+    	double markupAmount = (markupPercentage / 100) * originalPrice;
+    	double sellingPrice = originalPrice + markupAmount;
+
+    	// Calculate the sales tax
+    	double salesTax = (salesTaxRate / 100) * sellingPrice;
+
+    	// Calculate the final price including sales tax
+    	double finalPrice = sellingPrice + salesTax;
+
+    
+    	cout << "Original Price of the Item: $" << originalPrice << endl;
+    	cout << "Percentage Markup: " << markupPercentage << endl;
+    	cout << "Store's Selling Price: $" << sellingPrice << endl;
+    	cout << "Sales Tax Rate: " << salesTaxRate << endl;
+    	cout << "Sales Tax Amount: $" << salesTax << endl;
+    	cout << "Final Price of the Item: $" << finalPrice << endl;
+
+    	return 0;
+}
