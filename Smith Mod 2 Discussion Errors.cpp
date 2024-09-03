079754bf5e1fb8a62be0e15b6910dbe6f39c86e4
Connie Smith <connie.kay.smith@gmail.com>
2024-09-03 08:07:37 -0500

    Add files via upload
    Mod 2 Discussion - Debugging

---
+++ Smith Mod 2 Discussion Errors.cpp
@@ -0,0 +1,18 @@
+/*
+  This program gets a number from the user and displays that number.
+  */
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "You entered: " >> number << endl;
+
+    return 0;
+}
